2accf479d2e48fc084a6318f0e290b460325b179
Dimo Markov <dimomarkov@gmail.com>
2023-03-18 15:17:56 +0200

    Improved semantic concepts and tests

--- test/TestSemantics.cpp
+++ test/TestSemantics.cpp
@@ -8,31 +8,6 @@
 #include "Main.hpp"
 #include <catch2/catch.hpp>
 
-struct IncompleteType;
-
 SCENARIO("Testing semantics", "[semantics]") {
-   static_assert(not CT::DisownMakable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::DisownAssignable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::CloneMakable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::CloneAssignable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::AbandonMakable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::AbandonAssignable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::CopyMakable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::CopyAssignable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::MoveMakable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::MoveAssignable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::SemanticMakable<IncompleteType>,
-      "Can't make an incomplete type");
-   static_assert(not CT::SemanticAssignable<IncompleteType>,
-      "Can't make an incomplete type");
+
 }
