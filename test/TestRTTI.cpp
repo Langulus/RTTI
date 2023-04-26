8462b51f4647244548d188622280ff6591de120c
Epixu <dimomarkov@gmail.com>
2023-04-26 19:09:56 +0300

    Fixed test

--- test/TestRTTI.cpp
+++ test/TestRTTI.cpp
@@ -59,9 +59,9 @@ SCENARIO("Testing ambiguous symbols", "[ambiguity]") {
 			REQUIRE(found2.size() == 3);
 			REQUIRE(found2.find(vvv) != found2.end());
 			REQUIRE(found2.find(n1c) != found2.end());
-			REQUIRE(found3.size() == 9);
-			REQUIRE(found4.size() == 9);
-			REQUIRE(found5.size() == 9);
+			REQUIRE(found3.size() == 2);
+			REQUIRE(found4.size() == 2);
+			REQUIRE(found5.size() == 2);
 		}
 	}
 }
