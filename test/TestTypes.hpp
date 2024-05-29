f96e2c13cd2315abaa0c272f6b949885b7fb2b6b
Dimo Markov <dimomarkov@gmail.com>
2024-05-29 20:39:47 +0300

    Updated license

--- test/TestTypes.hpp
+++ test/TestTypes.hpp
@@ -1,3 +1,10 @@
+///                                                                           
+/// Langulus::RTTI                                                            
+/// Copyright (c) 2012 Dimo Markov <team@langulus.com>                        
+/// Part of the Langulus framework, see https://langulus.com                  
+///                                                                           
+/// SPDX-License-Identifier: MIT                                              
+///                                                                           
 #pragma once
 #include "Main.hpp"
 
